6bd0afd8723d0223d16eff7234ece3a378f416ad
AngelThread <ytr.ugur@gmail.com>
2016-10-22 13:49:00 +0300

    fix of ex41

--- ex41.py
+++ ex41.py
@@ -35,36 +35,36 @@ if len(sys.argv) == 2 and sys.argv[1] == "english":
 for word in urlopen(WORD_URL).readlines():
 	WORDS.append(word.strip())
 
-def convert (snippet, phrase):
-	class_names = [w.capitalize() for w in random.sample(WORDS, snippet.count("%%%"))]
-	other_names = random.sample(WORDS, snippet.count("***"))
-	results = []
-	param_names = []
-	print "snippet" ,snippet
-	
-	for i in range(0,snippet.count("@@@")):
-		param_count = random.randint(1,3)
-		param_names.append(',',join(random.sample(WORDS,param_count)))
-	
-	for sentence in snippet, phrase:
-		result = sentence[:]
-		
-		# fake class names
-	
-	for word in class_names:
-		result = result.replace("%%%",word,1)
-		
-	# fake other names
-	for word in other_names:
-		result = result.replace("***",word,1)
-	
-	# fake parameter lists
-	for word in param_names:
-		result = result.replace("@@@",word,1)
-		
-	results.append(result)
+def convert(snippet, phrase):
+    class_names = [w.capitalize() for w in
+                   random.sample(WORDS, snippet.count("%%%"))]
+    other_names = random.sample(WORDS, snippet.count("***"))
+    results = []
+    param_names = []
 
-	return results
+    for i in range(0, snippet.count("@@@")):
+        param_count = random.randint(1,3)
+        param_names.append(', '.join(random.sample(WORDS, param_count)))
+
+    for sentence in snippet, phrase:
+        result = sentence[:]
+
+        # fake class names
+        for word in class_names:
+            result = result.replace("%%%", word, 1)
+
+        # fake other names
+        for word in other_names:
+            result = result.replace("***", word, 1)
+
+        # fake parameter lists
+        for word in param_names:
+            result = result.replace("@@@", word, 1)
+
+        results.append(result)
+
+    return results
+
 	
 	# keep going until they hit CTRL -D
 try:
@@ -80,8 +80,8 @@ try:
 			phrase = PHRASES[snippet]
 			print "%d . phrase" % i
 			i = i + 1 
-			print "snippet--***??? ", snippet
-			print "phrase--***??? ", phrase
+			print "snippet--???:", snippet
+			print "phrase --???:", phrase
 
 			question, answer = convert(snippet, phrase)
 			if PHRASE_FIRST:

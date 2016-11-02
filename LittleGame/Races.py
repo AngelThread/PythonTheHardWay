85362d4b7091a2002c2f46fb05a44d7d90b21d46
Uğur Yeter <ytr.ugur@gmail.com>
2016-11-02 09:58:52 +0200

    Final Versions

--- LittleGame/Races.py
+++ LittleGame/Races.py
@@ -1,12 +1,41 @@
 class Races(object):
-	name = "Base Race"
-	
+	name = "Base"
+
+	def pickArace(self, number):
+		race = Races()
+		if number == 1:
+			return race
+		elif number == 2:
+			race = Ork()
+			return race
+		elif number == 3:
+			race = HalfHuman()
+			return race
+		elif number == 4:
+			race = ManKind()
+			return race
+		else: 
+			return None
 	
 	def tellMeMyRace(self):
-		print "Your race is %s" % Races.name
+		print "You are a %s" % Races.name
+
+
+class Ork(Races):
+	name = "Ork"
+
+	def tellMeMyRace(self):
+		print "You are an %s" % Ork.name
+
 
 class HalfHuman(Races):
-	name = "Half Human"
+	name = "Half Human and Half Ork"
 
 	def tellMeMyRace(self):
-		print "Your race is %s" % HalfHuman.name
+		print "You are a %s" % HalfHuman.name
+
+class ManKind(Races):
+	name = "Human"
+
+	def tellMeMyRace(self):
+		print "You are a %s" % ManKind.name

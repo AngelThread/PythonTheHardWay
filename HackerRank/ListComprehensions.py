3676d29c8b7f19ee1702a10c3c3eb60efb08aaf6
Uğur Yeter <ytr.ugur@gmail.com>
2016-11-12 22:36:30 +0200

    Solved

--- HackerRank/ListComprehensions.py
+++ HackerRank/ListComprehensions.py
@@ -3,17 +3,11 @@ x = int(raw_input().strip())
 y = int(raw_input().strip())
 z = int(raw_input().strip())
 n = int(raw_input().strip())
+ListOfThreeMultiples =[]
+for i in range(x+1):
+    for j in range(y+1):
+        for k in range(z+1) :
+            if i + j + k != n:
+                ListOfThreeMultiples.append([i,j,k])
 
-b =0 
-c =0
-ListOfNumbers = [[a , b , c ]for a in range(x+1)  if a + b + c != n] # List of integers from
-# for b in range(y) for c in range(z)
-bOfNumbers = [[a , b , c ]for b in range(y+1)  if a + b + c != n] # List of integers from
-
-cOfNumbers = [[a , b , c ]for c in range(z+1)  if a + b + c != n] # List of integers from
-
-ListOfNumbers.append(bOfNumbers)
-ListOfNumbers.append(cOfNumbers)
-ListOfNumbers.sort()
-
-print ListOfNumbers
+print ListOfThreeMultiples

0c445f7b9ae826a9a5e5427277f639097aec3177
Uğur Yeter <ytr.ugur@gmail.com>
2016-11-15 17:06:22 +0200

    gothonweb

--- projects/gothonweb/bin/app.py
+++ projects/gothonweb/bin/app.py
@@ -5,11 +5,11 @@ urls = (
 )
 
 app = web.application(urls,globals())
-
+render = web.template.render('templates/')
 class index:
 	def GET(self):
-		greeting = "Hello World"
-		return greeting
+		greeting = "Hello World!"
+		return render.index(greeting = greeting)
 
 if __name__ == "__main__":
 	app.run()

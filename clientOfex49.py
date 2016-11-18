87843a2a1fe66f0dcbdf641b8288c1b2694454df
Uğur Yeter <ytr.ugur@gmail.com>
2016-11-18 11:03:43 +0200

    Start of The Web Game

---
+++ clientOfex49.py
@@ -0,0 +1,15 @@
+import ex49
+
+listA = list()
+listA.append('d')
+listA.append('c')
+listA.append('b')
+listA.append('a')
+
+print "PEEK" , ex49.peek(listA)
+
+print "AFTER PEEK" , listA
+
+print "POP" , listA.pop(0)
+
+print "AFTER POP" , listA
